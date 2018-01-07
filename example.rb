df19213d5c8aa00dfbfb257aad6cb060f301a59d
Petter Fagerlund <petter.fagerlund@gmail.com>
2018-01-06 20:43:23 -0500

    test merge several branches

---
+++ example.rb
@@ -0,0 +1,14 @@
+This is where i want Petters code to This is where i want Petters code to
+This is where i want Petters code to This is where i want Petters code to This is where i want Petters code to
+This is where i want Petters code to This is where i want Petters code to This is where i want Petters code to This is where i want Petters code to This is where i want Petters code to This is where i want Petters code to
+This is where i want Petters code to This is where i want Petters code to This is where i want Petters code to This is where i want Petters code to This is where i want Petters code to This is where i want Petters code to This is where i want Petters code to
+This is where i want Petters code to
+This is where i want Petters code to
+This is where i want Petters code to
+This is where i want Petters code to
+This is where i want Petters code to
+This is where i want Petters code to
+This is where i want Petters code to
+This is where i want Petters code to
+This is where i want Petters code to
+This is where i want Petters code to 
